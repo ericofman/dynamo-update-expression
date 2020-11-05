db3b2453f85f033962b721ee4070980ef4dd69ad
Corey Forrieter <cforrieter@digitalmga.com>
2020-11-05 12:33:55 -0800

    refactor(prettier): import prettier settings for consistency with main project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const {
-    patches,
-    diff,
-    getUpdateExpression,
-    getVersionedUpdateExpression,
-    getVersionLockExpression
-} = require('./dynamo-update-expression');
+  patches,
+  diff,
+  getUpdateExpression,
+  getVersionedUpdateExpression,
+  getVersionLockExpression,
+} = require('./dynamo-update-expression')
 
 module.exports = {
-    patches,
-    diff,
-    getUpdateExpression,
-    getVersionedUpdateExpression,
-    getVersionLockExpression
-};
+  patches,
+  diff,
+  getUpdateExpression,
+  getVersionedUpdateExpression,
+  getVersionLockExpression,
+}
